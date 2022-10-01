038c097ea74524c49fa37fd9f18e5dd936c482d3
Arina <arinapostnikova123@gmail.com>
2022-10-01 13:39:25 +0300

    Initial commit

---
+++ myGame/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  myGame
+//
+//  Created by Arina Postnikova on 1.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
