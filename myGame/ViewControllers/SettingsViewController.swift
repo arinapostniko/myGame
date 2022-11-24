e350ef1531a5724a878d7b3317d8ca420883fb54
Arina <arinapostnikova123@gmail.com>
2022-11-24 17:50:13 +0300

    add deeplink

--- myGame/ViewControllers/SettingsViewController.swift
+++ myGame/ViewControllers/SettingsViewController.swift
@@ -6,10 +6,26 @@
 //
 
 import UIKit
+import MessageUI
 
 class SettingsViewController: UIViewController {
 
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - IBActions
+    @IBAction func reportButton(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["arinapostniko@icloud.com"])
+            mail.setSubject("Feedback")
+            mail.setMessageBody("<h>Report a problem</h>", isHTML: true)
+            mail.setCcRecipients(["arinapostnikova123@gmail.com"])
+            
+            present(mail, animated: true)
+        }
     }
 }
